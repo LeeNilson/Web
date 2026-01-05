160bdc2cebdfa1c363543af6dcc2cc753d169be2
Lee <99687743+LeeNilson@users.noreply.github.com>
2026-01-05 10:57:36 -0300

    Add voting and weekday example scripts
    Added ex011.js with voting eligibility and greeting logic based on age and time. Added ex013.js with a switch statement to display the current weekday name.

---
+++ Curso JS/aula12/ex011.js
@@ -0,0 +1,27 @@
+/*
+var idade = 66
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16){
+    console.log('Não vota')
+}else if (idade < 18 || idade > 65){
+       console.log('Voto Opcional')
+}else if (idade > 18){
+console.log('Voto Obrigatório')
+}
+*/
+
+/*
+var horaAtual = new Date()
+var hora = horaAtual.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 5){
+    console.log('Boa Madrugada!!')
+}else if (hora < 12){
+    console.log('Bom dia')
+}else if (hora < 18){
+    console.log('Boa Tarde')
+}else{
+    console.log('Boa Noite')
+}
+*/
+
