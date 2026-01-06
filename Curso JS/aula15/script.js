fb3546bb9400e64054d3c9341b37753aa61d9a05
Lee <99687743+LeeNilson@users.noreply.github.com>
2026-01-06 14:47:23 -0300

    Update age/gender detection UI and logic
    Improved the layout and styling in estilo.css for better alignment and image display. Added new images for elderly men and women. Updated index.html to add an ID to the verify button. Enhanced script.js to dynamically show age and gender with corresponding images based on user input.

--- Curso JS/aula15/script.js
+++ Curso JS/aula15/script.js
@@ -1,12 +1,41 @@
 function verificar(){
     var data = new Date ()
     var ano = data.getFullYear()
-    var fano = document.getElementById('txtano');
-    var res = document.querySelector('div#res');
-
-    if (fano.value.length == 0 || fano.value > ano ) {
-        window.alert('[ERRO] Verfique os dados e tente novamente!')
-    }else{
-        window.alert('Tudo ok!')
+    var fsex = document.getElementsByName('radsex')
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    var idade = ano - Number (fano.value) 
+    var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id','foto')
+    
+    if (fsex[0].checked) {
+        genero = 'Homem'
+        if (idade >=0 && idade < 10){
+            img.setAttribute('src', 'bebeMasculino.jpg')
+        } else if (idade < 21){
+            img.setAttribute('src', 'homemJovem.jpg')
+        } else if ( idade < 50 ){
+            img.setAttribute('src', 'homemAdulto.jpg')
+        } else{
+            img.setAttribute('src', 'homemIdoso.jpg')
+        }
     }
-}
+
+        if (fsex[1].checked) {
+        genero = 'Mulher'
+        if (idade >=0 && idade < 10){
+        img.setAttribute('src', 'bebeFeminino.jpg')
+        } else if (idade < 21){
+        img.setAttribute('src', 'mulherJovem.jpg')
+        } else if ( idade < 50 ){
+        img.setAttribute('src', 'mulherAdulta.jpg')
+        } else{
+        img.setAttribute('src', 'mulherIdosa.jpg')
+        }
+    }
+
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.` 
+    res.appendChild(img) 
+    }
