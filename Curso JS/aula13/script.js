35014816c0b6988aea29404f38c4a4a5d3e2da3e
Lee <99687743+LeeNilson@users.noreply.github.com>
2026-01-11 20:17:44 -0300

    Refactor script to use for loop for repetition
    Replaces previous function stubs and commented-out while/do-while loops with a for loop that prints 'Tudo Bem?' ten times. The change demonstrates different looping constructs for repetition in JavaScript.

--- Curso JS/aula13/script.js
+++ Curso JS/aula13/script.js
@@ -1,4 +1,22 @@
-function exibeResultado(){
-    console.log(log)
+/*
+console.log('Tudo Bem?')
+*/
+/*
+let c = 1
+    while (c <= 10){
+        console.log('Tudo Bem?')
+        c++
+    }
+*/
+/*
+c = 1
+do {
+    console.log('Tudo Bem?')
+    c++
+}while(c <=10)
+*/
+
+let c = 1
+for (let c =1;c<=10;c++){
+    console.log('Tudo Bem?')
 }
-function calcula(nome,calcula)
